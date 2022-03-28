e67add5caad2998bbdd77c12054d4666fd214d5e
Niels de Jong <niels-121@hotmail.com>
2022-03-28 16:57:11 +0200

    Cleanup of Cytoscape code

--- src/component/graph/Graph.tsx
+++ src/component/graph/Graph.tsx
@@ -16,28 +16,18 @@ var defaults = {
 
 var navigator = require('cytoscape-navigator');
 
-
-
 const GraphVisualization = ({ elements = [], disabled = false }) => {
     const [ref, setRef] = useState(undefined);
 
     if(ref){
-        console.log(ref)
         navigator( cytoscape ); // register extension
-        var cy = cytoscape({
-            container: ref // container to render in
-          });
-        var eles = cy.add([
-        { group: 'nodes', data: { id: 'n0' }, position: { x: 100, y: 100 } },
-        { group: 'nodes', data: { id: 'n1' }, position: { x: 200, y: 200 } },
-        { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } }
-        ]);    
+        var cy = cytoscape({ container: ref });
+        var eles = cy.add(elements);    
         var nav = cy.navigator( defaults ); // get navigator instance, nav
     }
 
     return <div style={{ width: "100%", height: "500px" }}>
-        <p> I am a title {disabled} </p>
-      <div id="cy" ref={(cyRef) => { setRef(cyRef); }} style={{ width: "100%", height: "300px" }}> </div>
+      <div id="cy" ref={(cyRef) => { setRef(cyRef); }} style={{ width: "100%", height: "100%" }}> </div>
      </div>
 }
 
