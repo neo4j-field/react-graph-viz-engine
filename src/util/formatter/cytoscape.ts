f2fa58c5e9febb67566f02eaf81f2b3bdb0bc689
Marius Conjeaud <marius.conjeaud@neo4j.com>
2022-03-29 14:01:29 +0200

    Fix formatter

--- src/util/formatter/cytoscape.ts
+++ src/util/formatter/cytoscape.ts
@@ -3,22 +3,20 @@ import { GraphNode, GraphEdge } from "../parser/parser";
 export function formatData(data: [GraphNode[], GraphEdge[]]): object {
     var [nodes, edges] = data
     var elements = {
-        elements: {
-            nodes: [],
-            edges: []
-        }
+        nodes: [],
+        edges: []
     }
     nodes.forEach(function (node) {
         var element = { ...node, ...node["properties"] }
         delete element["properties"]
         //@ts-ignore
-        elements["elements"]["nodes"].push({ data: element })
+        elements["nodes"].push({ data: element })
     });
     edges.forEach(function (edge) {
         var element = { ...edge, ...edge["properties"] }
         delete element["properties"]
         //@ts-ignore
-        elements["elements"]["edges"].push({ data: element })
+        elements["edges"].push({ data: element })
     });
     console.log(elements)
     return elements
