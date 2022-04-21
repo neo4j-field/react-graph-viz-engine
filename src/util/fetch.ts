dc415a864b4714ebc6e9382b5371930e9f70cc8f
johnymontana <lyonwj@gmail.com>
2022-04-20 20:32:12 -0600

    explicitly set content-type header

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,5 +1,8 @@
 export async function fetchGraphQLDataJSON(url: string, query: string) {
     const response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
         method: 'POST',
         body: JSON.stringify({query: query})
       });
